3ff7d84b5be15c16f17a42d83f9c6b16863a89fd
Guido <guidocarda@hotmail.com>
2023-03-20 12:38:55 +0000

    Simple Button test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import clsx from "clsx";
 import "./App.css";
 
 function App() {
-  return <div className="bg-indigo-400 h-screen"></div>;
+  return (
+    <div className="bg-indigo-400 h-screen grid place-items-center">
+      <Button
+        className={clsx(
+          "h-10 px-4 rounded-md bg-neutral-300 border-2 border-transparent",
+          "hover:bg-neutral-200",
+          "active:border-3 active:border-neutral-500/10"
+        )}
+      >
+        Hola
+      </Button>
+    </div>
+  );
 }
 
 export default App;
+
+const Button = ({ children, ...props }) => {
+  return <button {...props}>{children}</button>;
+};
